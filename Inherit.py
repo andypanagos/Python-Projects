6a35fc07114b5fdb72b6bb144a50d2550f0beb18
andypanagos <andypanagos2@gmail.com>
2022-09-19 16:25:23 -0400

    Create Inherit.py

---
+++ Inherit.py
@@ -0,0 +1,12 @@
+class Car:
+    make = 'chevy'
+    model = 'equinox'
+    year = 2018
+
+class Engine(Car):
+    cylinders = 6
+    horsepower = 306
+
+class Interior(Car):
+    infotainment = 'applecarplay'
+    seat_type = 'leather'
